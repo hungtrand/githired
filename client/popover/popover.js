35b109c45fe1bb2e275351d7e891a3114136c251
bhargavat <komalis@Bhargavas-MBP-5.T-mobile.com>
2016-03-14 01:25:34 -0700

    Minor enhancements to popover

--- client/popover/popover.js
+++ client/popover/popover.js
@@ -1,17 +1,18 @@
-    $( function() /* popover action */
-    {
-    	$("[data-toggle=popover]").popover({
-      html: true, 
-      content: function() {
-          return $('#popover-content').html();
-        }
-      });
-    } );
-
-    $('body').on('click', function (e) { /* closes popover when clicked outside of it */
-    //only buttons
-    if ($(e.target).data('toggle') !== 'popover'
-        && $(e.target).parents('.popover.in').length === 0) { 
-        $('[data-toggle="popover"]').popover('hide');
+$( function() {
+  $("[data-toggle=popover]").popover({
+    html: true, 
+    content: function() {
+      return $('#popover-content').html();
     }
+  });
+});
+
+$('body').on('click', function (e) {
+  if (
+    $(e.target).data('toggle') !== 'popover' 
+    && !$(e.target).parents('.popover.in').length 
+    && $('.popover.in').length
+  ) { 
+    $("[data-toggle=popover]").trigger('click');
+  }
 });
