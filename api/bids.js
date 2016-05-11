147774d58c0d0ad710ef7d46f04245d301a7c54f
Kyaw <skyversal@gmail.com>
2016-05-10 17:29:55 -0700

    added few feature for viewing bids from employee side

--- api/bids.js
+++ api/bids.js
@@ -37,14 +37,24 @@ router.post("/:userId/bids", function(req, res, next) {
 });
 
 
-//user views currents bids
-router.get("/:jobId/currentbids", function(req, res, next){
+//employer views currents bids
+/**
+ *  @api {get} /api/user/jobs/:jobId/currentbids viewing current bids amount and users for job with jodid.
+ *  @apiName GetCurrentBids
+ *  @apiGroup currentBids
+ *  @apiVesion 1.0.0
+ *  @api
+ *  @apiParam {number} jobId jobId
+ */
+router.get("/jobs/:jobId/currentbids", function(req, res, next){
+    res.setHeader('Content-Type', 'application/json');
     var jobId = req.param('jobId');
     var viewbids = bidsContext.findAll({
         where: {jobid: jobId}, 
         include: [{model: userContext
+            , attributes: ['userId','firstName', 'lastName', 'company', 'email', 'linkedin']
         }, 
-        {model: jobContext}]
+        {model: jobContext, attributes: ['jobId','jobTitle', 'jobDescription', 'minimumWage', 'setWage', 'jobType', 'position', 'startingDate']}]
 
 
     }).then(function(bids){
@@ -58,16 +68,51 @@ router.get("/:jobId/currentbids", function(req, res, next){
     });
 });
 
-//update job bids
-router.put("/:userId/currentbid/:bidId/updatebid", function(req,res,next){
+
+//employee views his/her current bid on specified job.
+router.get("/:userId/jobs/:jobId/currentbid/:bidId", function(req, res, next){
+    res.setHeader('Content-Type','application/json');
     var userId = req.param('userId');
+    var jobId = req.param('jobId');
     var bidId = req.param('bidId');
+    var reviewCurrentBid = bidsContext.findOne({
+        where: {
+            userId: userId
+            ,jobId: jobId
+            ,bidId: bidId
+        }, 
+        attributes: ['amount', 'createdAt', 'updatedAt'],
+        include: [{model: jobContext 
+            , attributes: ['jobTitle','jobDescription', 'minimumWage'
+            , 'maximumWage', 'jobType', 'position', 'startingDate', 'location'],
+            include: [{model: userContext, 
+                attributes: ['firstName', 'lastName', 'company', 'email', 'linkedin']}]
+        }]
+    }).then(function(bid){
+        if(bid){
+            res.send(JSON.stringify(bid));
+        }else{
+            res.sendStatus(401);
+        }
+    }).catch(function(err){
+        res.send(JSON.stringify(err));
+    });
+});
+
+//update job bids
+router.put("/:userId/jobs/:jobId/currentbid/:bidId/updatebid", function(req,res,next){
+    var userId = req.param('userId');
+    var jobId = req.param('jobId');
+    var bidId = req.param('bidId');
+    var utime = Date.now();
     var updatebid = bidsContext.update(
-        {amount: req.body["amount"]},
-    {
+        {amount: req.body["amount"]
+        ,updatedAt: utime}
+        , {
         
         where:{
             userId: userId,
+            jobId: jobId,
             bidId: bidId
         }
         
